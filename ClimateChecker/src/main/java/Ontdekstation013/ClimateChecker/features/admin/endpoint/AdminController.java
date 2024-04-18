a187efeec38dbe012a04ebc1fe37bfbeb7ec454e
Jesse Kraus <93532284+KrausJ2002@users.noreply.github.com>
2024-04-18 11:30:04 +0200

    gefixt!

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/admin/endpoint/AdminController.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/admin/endpoint/AdminController.java
@@ -5,6 +5,7 @@ import Ontdekstation013.ClimateChecker.features.admin.WorkshopCode;
 import Ontdekstation013.ClimateChecker.features.admin.WorkshopCodeRepository;
 
 
+import Ontdekstation013.ClimateChecker.features.admin.wsCodeRequest;
 import Ontdekstation013.ClimateChecker.features.authentication.JWTService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +32,23 @@ public class AdminController {
         this.jwtService = jwtService;
     }
 
-    @GetMapping("createworkshopcode")
-    @CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
-    public ResponseEntity<Void> createNewWorkshopCode(@RequestParam Long duration, @RequestParam Long length) {
-        WorkshopCode workshopCode = adminService.createWorkshopCode(duration, length);
+//    @GetMapping("createworkshopcode")
+//    @CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
+//    public ResponseEntity<Void> createNewWorkshopCode(@RequestParam Long duration, @RequestParam Long length) {
+//        WorkshopCode workshopCode = adminService.createWorkshopCode(duration, length);
+//        if (workshopCode != null) {
+//            return ResponseEntity.status(HttpStatus.OK).body(null);
+//        }
+//        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null);
+//    }
+
+    @PostMapping("createworkshopcode")
+    public ResponseEntity<Void> createNewWorkshopCode(@RequestBody wsCodeRequest request) {
+        WorkshopCode workshopCode = adminService.createWorkshopCode(request.getDuration(), request.getLength());
         if (workshopCode != null) {
-            return ResponseEntity.status(HttpStatus.OK).body(null);
+            return ResponseEntity.status(HttpStatus.CREATED).body(null);
         }
-        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
+
 }
