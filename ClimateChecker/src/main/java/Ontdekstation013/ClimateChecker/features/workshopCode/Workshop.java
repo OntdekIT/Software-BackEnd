ba280d50837174036baad812444c1a991a92e9aa
Arjan Gosens <a.gosens@student.fontys.nl>
2024-11-13 13:51:17 +0100

    Fixed validation package imports

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/workshopCode/Workshop.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/workshopCode/Workshop.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
