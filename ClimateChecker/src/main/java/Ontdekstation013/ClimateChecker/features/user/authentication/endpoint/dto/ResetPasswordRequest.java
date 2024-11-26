76b055c014520fefbdad40479b25f0355b1fc712
Arjan Gosens <a.gosens@student.fontys.nl>
2024-11-26 14:24:29 +0100

    Improved global exception handling, added password validation to ResetPasswordRequest

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/user/authentication/endpoint/dto/ResetPasswordRequest.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/user/authentication/endpoint/dto/ResetPasswordRequest.java
@@ -1,6 +1,7 @@
 package Ontdekstation013.ClimateChecker.features.user.authentication.endpoint.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record ResetPasswordRequest(
 
@@ -8,6 +9,7 @@ public record ResetPasswordRequest(
         String email,
 
         @NotBlank
+        @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$", message = "must be at least 8 characters long, contain at least one capital letter and one number")
         String password,
 
         @NotBlank
