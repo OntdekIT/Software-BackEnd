21587fa12b22bafc2bd943750e3d2b08f994e1f7
Arjan Gosens <a.gosens@student.fontys.nl>
2024-12-10 14:21:20 +0100

    Fixed MyAccountController updateUser endpoint not updating user correctly

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/user/endpoint/MyAccountController.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/user/endpoint/MyAccountController.java
@@ -1,23 +1,23 @@
 package Ontdekstation013.ClimateChecker.features.user.endpoint;
 
+import Ontdekstation013.ClimateChecker.features.user.PasswordEncodingService;
 import Ontdekstation013.ClimateChecker.features.user.User;
 import Ontdekstation013.ClimateChecker.features.user.UserMapper;
 import Ontdekstation013.ClimateChecker.features.user.UserService;
 import Ontdekstation013.ClimateChecker.features.user.endpoint.dto.UpdateMyAccountRequest;
 import Ontdekstation013.ClimateChecker.features.user.endpoint.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/my-account")
 public class MyAccountController {
     private final UserService userService;
-
-    public MyAccountController(UserService userService) {
-        this.userService = userService;
-    }
+    private final PasswordEncodingService passwordEncodingService;
 
     @GetMapping()
     public ResponseEntity<UserResponse> getUser(@AuthenticationPrincipal UserDetails userDetails, @RequestParam(defaultValue = "false") boolean includeStations) {
@@ -26,9 +26,16 @@ public class MyAccountController {
     }
 
     @PutMapping()
-    public ResponseEntity<?> updateUser(@AuthenticationPrincipal UserDetails userDetails, @RequestBody UpdateMyAccountRequest updateMyAccountRequest) {
+    public ResponseEntity<?> updateUser(@AuthenticationPrincipal UserDetails userDetails, @RequestBody UpdateMyAccountRequest updateRequest) {
         User user = userService.getUserById(Long.parseLong(userDetails.getUsername()));
-        user = UserMapper.toUpdatedUser(user, updateMyAccountRequest);
+        user.setEmail(updateRequest.email());
+        user.setFirstName(updateRequest.firstName());
+        user.setLastName(updateRequest.lastName());
+
+        if (updateRequest.password() != null && !updateRequest.password().isEmpty()) {
+            user.setPassword(passwordEncodingService.encodePassword(updateRequest.password()));
+        }
+
         userService.updateUser(user.getUserId(), user);
         return ResponseEntity.ok().build();
     }
