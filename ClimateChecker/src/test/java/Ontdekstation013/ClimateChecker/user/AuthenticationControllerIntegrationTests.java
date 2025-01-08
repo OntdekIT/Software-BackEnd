26d66bd184f483b6a288533a4bec73a4de800fd0
Arjan Gosens <a.gosens@student.fontys.nl>
2025-01-08 10:40:51 +0100

    Fixed integration test config (still fails due to db schema issues)

--- ClimateChecker/src/test/java/Ontdekstation013/ClimateChecker/user/AuthenticationControllerIntegrationTests.java
+++ ClimateChecker/src/test/java/Ontdekstation013/ClimateChecker/user/AuthenticationControllerIntegrationTests.java
@@ -1,18 +1,22 @@
 package Ontdekstation013.ClimateChecker.user;
 
+import Ontdekstation013.ClimateChecker.ClimateCheckerApplication;
 import Ontdekstation013.ClimateChecker.features.user.authentication.endpoint.dto.RegisterUserRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = ClimateCheckerApplication.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Sql(scripts = "/schema.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
--- ClimateChecker/src/test/resources/schema.sql
+++ ClimateChecker/src/test/resources/schema.sql
@@ -1,27 +1,27 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     user_role VARCHAR(50)
-);
+    );
 
-CREATE TABLE workshop (
+CREATE TABLE IF NOT EXISTS workshop (
     code BIGINT PRIMARY KEY,
     expiration_date DATETIME,
     creation_date DATETIME
 );
 
-CREATE TABLE station (
+CREATE TABLE IF NOT EXISTS station (
     stationid BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     database_tag VARCHAR(255),
-    is_public BIT(1),
-    registration_code BIGINT(20),
+    is_public BOOLEAN,
+    registration_code BIGINT,
     user_id BIGINT,
-    is_active BIT(1)
-);
+    is_active BOOLEAN
+    );
 
 INSERT INTO workshop (code, expiration_date, creation_date) VALUES (123456, '2025-12-31 13:44:22.000', '2024-11-12 15:54:13.000');
 
