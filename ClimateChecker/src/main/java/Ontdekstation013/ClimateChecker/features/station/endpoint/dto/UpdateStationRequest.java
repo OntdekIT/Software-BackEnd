193f6161ccf2011fe6b70c3349fd5e6dff1da4cc
Yassien <y.boughlala@sutdent.fontys.nl>
2025-02-14 10:10:59 +0100

    Added Feature edit meetstation

---
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/station/endpoint/dto/UpdateStationRequest.java
@@ -0,0 +1,11 @@
+package Ontdekstation013.ClimateChecker.features.station.endpoint.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateStationRequest(
+        @NotBlank
+        String name,
+        @NotBlank
+        Boolean is_public
+) {
+}
